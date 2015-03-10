8ddfd57c5a5c732c12f81125e9a03325b8742fff
Felipe Kaufmann <felipekaufmann@gmail.com>
2015-03-10 15:27:09 +0100

    override path to migrations
    See rspec/rspec-rails#1330

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ FactoryGirl.find_definitions
 ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.default_url_options[:host] = 'test.com'
+ActiveRecord::Migrator.migrations_paths = 'spec/dummy/db/migrate'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
