cd3c1ff0cc7551117d5002817478c3facf4d5587
kuschti <kuschti@gmail.com>
2015-03-18 16:30:09 +0100

    add partystreusel styling
    - style header
    - style pages and pages/new
    - add some partytreusel styles

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,14 +1,12 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.wrappers :bootstrap, tag: 'div', class: 'control-group', error_class: 'error' do |b|
+  config.wrappers :'form--horizontal', tag: 'p', class: 'form--horizontal', error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
-    b.wrapper tag: 'div', class: 'controls' do |ba|
-      ba.use :input
-      ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
-      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
-    end
+    b.use :input
+    b.use :error, wrap_with: { tag: 'span', class: 'form__help-inline' }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'form__help-block' }
   end
 
   config.wrappers :prepend, tag: 'div', class: 'control-group', error_class: 'error' do |b|
@@ -42,4 +40,5 @@ SimpleForm.setup do |config|
   # to learn about the different styles for forms and inputs,
   # buttons and other elements.
   config.default_wrapper = :bootstrap
+  config.boolean_style = :nested
 end
