ee1a450617c1cb0bdf7f02ef8bb186d1f1242576
kuschti <kuschti@gmail.com>
2015-03-25 15:33:14 +0100

    add pages, settings & form styling
    - add page edit styling
    - add default form styling
    - add settings styling
    - change bootstrap simple-form to partystreusel simple-form

--- config/initializers/simple_form_partystreusel.rb
+++ config/initializers/simple_form_partystreusel.rb
@@ -1,6 +1,6 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.wrappers :'form--horizontal', tag: 'p', class: 'form--horizontal', error_class: 'error' do |b|
+  config.wrappers :'partystreusel', tag: 'p', class: 'form--horizontal', error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
@@ -39,6 +39,6 @@ SimpleForm.setup do |config|
   # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
   # to learn about the different styles for forms and inputs,
   # buttons and other elements.
-  config.default_wrapper = :bootstrap
+  config.default_wrapper = :partystreusel
   config.boolean_style = :nested
 end
