130049079b84edf64c52e384a4c350ea145477f2
Noëlle Rosenberg <rosenberg.noelle@gmail.com>
2015-08-05 09:45:55 +0200

    throw exception if admin was not found

--- app/controllers/qbrick/cms/admins_controller.rb
+++ app/controllers/qbrick/cms/admins_controller.rb
@@ -67,7 +67,7 @@ module Qbrick
 
       # Use callbacks to share common setup or constraints between actions.
       def set_admin
-        @admin = Qbrick::Admin.find_by id: params[:id]
+        @admin = Qbrick::Admin.find params[:id]
       end
 
       # Never trust parameters from the scary internet, only allow the white list through.
